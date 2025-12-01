307f9a8fb9c83318d557b8f32716195f0560bca0
toniramss <toni.rams.sebastia@gmail.com>
2025-12-01 17:54:47 +0100

    Mejorar la impresión de estadísticas para los conjuntos de datos de vino tinto y blanco, incluyendo descripciones y conteos de nulos y duplicados.

--- main.py
+++ main.py
@@ -9,18 +9,26 @@ df_white_wine = pd.read_csv(irl_white_wine, sep=';')
 print(df_red_wine.head())
 
 
-print("Describe")
+print("Describe red wine")
 print(df_red_wine.describe())
+
+print("Describe white wine")
+print(df_white_wine.describe())
 
 print("\nInfo")
 print(df_red_wine.info())
 print(df_white_wine.info())
 
-print("Cantidad de nulos por columna")
+print("Cantidad de nulos por columna red wine")
 print(df_red_wine.isnull().sum())
 
-print("\nCantidad de duplicados")
+print("Cantidad de nulos por columna white wine")
+print(df_white_wine.isnull().sum())
+
+print("\nCantidad de duplicados red wine")
 print(df_red_wine.duplicated().sum())
+print("\nCantidad de duplicados white wine")
+print(df_white_wine.duplicated().sum())
 
 
 # Mostrar filas duplicadas (posible informe)
@@ -31,9 +39,14 @@ print(duplicates_sorted)
 
 #Eliminar las filas duplicadas
 df_red_wine = df_red_wine.drop_duplicates()
+df_white_wine = df_white_wine.drop_duplicates()
 
 # Quitar espacios en nombres de columnas
 df_red_wine.columns = df_red_wine.columns.str.strip().str.lower().str.replace(' ', '_')
 print(df_red_wine.columns)
 df_white_wine.columns = df_white_wine.columns.str.strip().str.lower().str.replace(' ', '_')
 print(df_white_wine.columns)
+
+
+print(len(df_red_wine))
+print(len(df_white_wine))

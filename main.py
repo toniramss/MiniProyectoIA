ab6383d4393ded1075c1cece88a8c8946aaafb90
Pau Blanco <paublaber@campus.monlau.com>
2025-12-01 16:46:38 +0100

    Importar CSV's

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import pandas as pd
+
+url_red_wine = "https://raw.githubusercontent.com/shrikant-temburwar/Wine-Quality-Dataset/refs/heads/master/winequality-red.csv"
+irl_white_wine = "https://raw.githubusercontent.com/shrikant-temburwar/Wine-Quality-Dataset/refs/heads/master/winequality-white.csv"
+
+df_red_wine = pd.read_csv(url_red_wine, sep=';')
+df_white_wine = pd.read_csv(irl_white_wine, sep=';')
+
+df_red_wine.head()
